e8734aecd169483799962770f1089bed17272a9c
Maksim Sokolski <misokolsky@gmail.com>
2014-03-17 12:09:48 +0200

    Initial commit.

---
+++ README.rst
@@ -0,0 +1,32 @@
+=======
+Wowstat
+=======
+
+Wowsat is a simple Django app to conduct Web-based Wowza server statistic.
+
+Quick start
+-----------
+
+1. Add "wowstat" to your INSTALLED_APPS setting like this::
+
+       INSTALLED_APPS = (
+          ...
+          'wowstat',
+       )
+
+2. Include the polls URLconf in your project urls.py like this::
+
+    url(r'^wowstat/', include('wowstat.urls')),
+
+3. Create or change conf.ini file in root of your django project to add next:
+   [wowza]
+   server_ip = 
+   server_port = 
+   login = 
+   password =
+
+   [postgresql]
+   ip = 
+   user = 
+   pass = 
+
---
+++ setup.py
@@ -0,0 +1,30 @@
+import os
+from setuptools import setup
+
+README = open(os.path.join(os.path.dirname(__file__), 'README.rst')).read()
+
+os.chdir(os.path.normpath(os.path.join(os.path.abspath(__file__), os.pardir)))
+
+setup(
+        name='django-wowstat',
+        version='0.1',
+        packages=['wowstat'],
+        include_package_data=True,
+        license='The MIT License: http://www.opensource.org/licenses/mit-license.php',
+        description='A simple Django app to conduct statistic from Wowza server.',
+        long_description=README,
+        author='Maksym Sokolsky',
+        author_email='misokolsky@gmail.com',
+        classifiers=[
+                    'Environment :: Web Environment',
+                    'Framework :: Django',
+                    'Intended Audience :: Developers',
+                    'License :: OSI Approved :: MIT License', # example license
+                    'Operating System :: OS Independent',
+                    'Programming Language :: Python',
+                    'Programming Language :: Python :: 2',
+                    'Programming Language :: Python :: 2.7',
+                    'Topic :: Internet :: WWW/HTTP',
+                    'Topic :: Internet :: WWW/HTTP :: Dynamic Content',
+                ],
+)
