66e6fb6ce3fdb4d85c3576dbb54ac36f1923fb57
Maksim Sokolskiy <m_sokolskiy@wargaming.net>
2014-06-17 14:16:09 +0300

    Add docstring to WowzaConnections.

--- wowstat/models.py
+++ wowstat/models.py
@@ -2,5 +2,11 @@ from django.db import models
 from datetime import datetime
 
 class WowzaConnections(models.Model):
+    """
+    WowzaConnections(query_time=datetime.now, conn_counts)
+
+    Model include creating time and connections count.
+    Default value of query_time is datetime.now
+    """
     query_time = models.DateTimeField(default=datetime.now)
     conn_counts = models.IntegerField()
